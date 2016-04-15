f3a43c050c30bfc25e26f2e9e53b41dd0fe43dc8
Pavel Stratan <pstratan@salesforce.com>
2016-04-15 11:01:50 +0100

    video 5 exercise completed

--- app/components/Main.js
+++ app/components/Main.js
@@ -3,9 +3,9 @@ var React = require('react');
 var Main = React.createClass({
   render: function () {
     return (
-      <div className ='main-container'>
-      {this.props.children} 
-      </div> 
+      <div className='main-container'>
+        {this.props.children}
+      </div>
     )
   }
 });
